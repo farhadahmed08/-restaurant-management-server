43a3ec59681dbef9cc628b98711da9eb837c3318
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-12-04 02:17:38 +0800

    env file updated

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 
-
-
-
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ObjectId,ServerApiVersion } = require("mongodb");
@@ -15,16 +12,17 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const uri = "mongodb://0.0.0.0:27017/";
+// const uri = "mongodb://0.0.0.0:27017/";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri);
-// const uri = "mongodb+srv://farhad08:rNhsca82vZJgKGAD@cluster0.qaohrfy.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qaohrfy.mongodb.net/?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri);
-
-
-
+const client = new MongoClient(uri,{
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 
 
